073eb00ddf22551374d78dce10ecfb6bab7c73fe
BogdanV <zarazailo@gmail.com>
2022-06-29 20:33:10 +0300

    fix1

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,7 +4,7 @@ import s from './Loader.module.css';
 export default function Loader() {
   return (
     <div className={s.overlay}>
-      <DotLoader size={250} color={'#461646'} className={s.loader} />
+      <DotLoader size={250} color={'#003399'} className={s.loader} />
     </div>
   );
 }
